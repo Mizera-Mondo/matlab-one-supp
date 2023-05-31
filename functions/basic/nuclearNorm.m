67faea4975df3baf1aba528e7529b88e753bbec6
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-05-31 19:13:05 +0800

    fix(basic): reimplement of nuclear norm. use SVD instead of direct calculation, faster, more stable and fix the bug of inlcuding imaginary part in the result

--- functions/basic/nuclearNorm.m
+++ functions/basic/nuclearNorm.m
@@ -1,5 +1,7 @@
 function n = nuclearNorm(M)
 %NUCLEARNORM Calculate the nuclear norm n of matrix M.
-n = trace((M'*M)^(1/2));
+% n = trace((M'*M)^(1/2));
+[~, S, ~] = svd(M);
+n = sum(diag(S));
 end
 
