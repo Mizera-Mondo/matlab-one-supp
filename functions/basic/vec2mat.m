b27c16e72ec20ef0993fd95912b345e24787f7ee
Avrona <127651428+Mizera-Mondo@users.noreply.github.com>
2023-06-05 15:03:43 +0800

    fix: redo of vec2mat.m

--- functions/basic/vec2mat.m
+++ functions/basic/vec2mat.m
@@ -1,11 +1,17 @@
 function M = vec2mat(v, r)
-%VEC2MAT 此处显示有关此函数的摘要
-%   此处显示详细说明
+%VEC2MAT Reshape a vector to a matrix.
+%   r is the number of rows of the new matrix and the number of column will
+%   be automatically detected.
 [a,b] = size(v);
-s = a/r;
-if b == 1 && s == round(s)
-    M = reshape(v,r,s);
+
+if b == 1 && mod(a, r) == 0
+    M = reshape(v, r, a/r);
 else
     M = 0;
+    if b ~= 1
+        error('Input must be a vector.');
+    else 
+        error('Could not construct a %d-row-matrix with input vector.', r);
+    end
 end
 end
